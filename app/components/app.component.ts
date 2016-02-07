086d209558cf9a065084ab96fd11505f9ebc6619
Sheldon Barnes <sheldonbarnes@icloud.com>
2016-02-07 17:59:39 -0500

    Initial commit
    Initial commit for Angular2 TypeScript class

---
+++ app/components/app.component.ts
@@ -0,0 +1,17 @@
+import {Component, View} from 'angular2/core';
+
+@Component({
+    selector: 'my-app'
+})
+
+@View({
+    template:
+    `
+<h1>Hello World</h1>
+    `
+})
+
+export class AppComponent {
+  constructor(){
+  }
+}
